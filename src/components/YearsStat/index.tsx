6547c003028b412a2989fb0e6126f302e617f1eb
Frank_Woon <wenfp108@gmail.com>
2024-05-21 22:34:31 +0800

    change

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -1,3 +1,4 @@
+
 import YearStat from '@/components/YearStat';
 import useActivities from '@/hooks/useActivities';
 import { INFO_MESSAGE } from '@/utils/const';
@@ -12,11 +13,15 @@ const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) =
 
   // for short solution need to refactor
   return (
-    <div className="w-full lg:w-full pb-16 pr-16 lg:pr-16">
-      <section className="pb-0">
-        <p className="leading-relaxed">
+    <div className="fl w-100-l pb5 pr5-l">
+      <section className="pb4" style={{ paddingBottom: '0rem' }}>
+        <p style={{ lineHeight: 1.8 }}>
           {INFO_MESSAGE(years.length, year)}
           <br />
+          <br />
+          "明明这么痛苦，这么难过，为什么就是不能放弃跑步？因为全身细胞都在蠢蠢欲动，想要感受强风迎面吹拂的滋味。"
+          <br />
+          <p style={quoteStyle}>&ndash;&ndash;《强风吹拂》</p>
         </p>
       </section>
       <hr color="red" />
@@ -31,6 +36,11 @@ const YearsStat = ({ year, onClick }: { year: string, onClick: (_year: string) =
         )}
     </div>
   );
+};
+
+const quoteStyle = {
+    fontWeight:"bold",
+    textAlign: "right"
 };
 
 export default YearsStat;
--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'Woon跑步日志',
+  siteUrl: 'https://www.strava.com/athletes/135469581',
+  logo:  'https://i.ibb.co/GMqKV7L/20240429191059.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'STRAVA资料',
+      url: 'https://www.strava.com/athletes/135469581',
     },
   ],
 };
